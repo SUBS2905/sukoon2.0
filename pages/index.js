70304c09818af97d01e309162ebee1f339ecd5d2
Subhransu <subhranshumajhi2@gmail.com>
2023-08-13 02:04:14 +0530

    add content

--- pages/index.js
+++ pages/index.js
@@ -25,23 +25,26 @@ export default function Home() {
               />
             </div>
             <div className="w-1/2 flex flex-col items-center self-center">
-              <h1 className="inline-block w-full text-dark font-bold capitalize text-6xl">
-                Heading goes here
+              <h1 className="inline-block w-full text-dark font-bold text-6xl">
+                Breaking Barriers
               </h1>
+              <h3 className="inline-block w-full pt-2 font-semibold text-gray-700 text-xl">
+                Your Bridge to Confidential Online Mental Health Support
+              </h3>
               <p className="my-4 text-base font-medium">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc
-                faucibus a pellentesque sit amet porttitor eget dolor. Ac ut
-                consequat semper viverra nam libero justo laoreet. Proin libero
-                nunc consequat interdum varius. Dui accumsan sit amet nulla
-                facilisi morbi tempus iaculis urna.
+                Welcome to Sukoon, where we believe that taking care of your
+                mental health should be as accessible as a click away.
+                Adolescence can be a challenging time, and we&apos;re here to
+                provide you with the support and tools you need to navigate
+                these hurdles with resilience and strength.
               </p>
               <p className="my-4 text-base font-medium">
-                Id faucibus nisl tincidunt eget nullam non nisi. Risus feugiat
-                in ante metus dictum at tempor commodo. Ut sem viverra aliquet
-                eget sit amet tellus. Duis at consectetur lorem donec massa
-                sapien faucibus et. Gravida arcu ac tortor dignissim convallis.
-                Mauris sit amet massa vitae tortor condimentum lacinia quis vel.
+                We understand that seeking help for mental health
+                concerns can be daunting. That&apos;s why we&apos;ve created a
+                safe and confidential online space where you can open up without
+                fear of judgment or embarrassment. Our expert professionals are
+                here to listen, guide, and empower you on your path to mental
+                wellness.
               </p>
               <div className="flex items-center self-start mt-2">
                 <Link
@@ -57,23 +60,26 @@ export default function Home() {
         <LayoutDark className="pt-32 text-white">
           <div className="flex items-center justify-between w-full">
             <div className="w-1/2 flex flex-col items-center self-center">
-              <h1 className="inline-block w-full text-dark font-bold capitalize text-6xl">
-                Heading goes here
+              <h1 className="inline-block w-full text-dark font-bold text-6xl">
+                Courage to Heal
               </h1>
+              <h3 className="inline-block w-full pt-2 font-semibold text-gray-300 text-xl">
+                Find Solace and Support on Our Digital Platform
+              </h3>
               <p className="my-4 text-base font-medium">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc
-                faucibus a pellentesque sit amet porttitor eget dolor. Ac ut
-                consequat semper viverra nam libero justo laoreet. Proin libero
-                nunc consequat interdum varius. Dui accumsan sit amet nulla
-                facilisi morbi tempus iaculis urna.
+                Healing requires courage, and Sukoon is here to honor that
+                courage by providing a digital refuge of solace and support. We
+                understand that reaching out for help can be intimidating, which
+                is why we&apos;ve created a space where you can be heard,
+                understood, and guided towards a path of healing.
               </p>
               <p className="my-4 text-base font-medium">
-                Id faucibus nisl tincidunt eget nullam non nisi. Risus feugiat
-                in ante metus dictum at tempor commodo. Ut sem viverra aliquet
-                eget sit amet tellus. Duis at consectetur lorem donec massa
-                sapien faucibus et. Gravida arcu ac tortor dignissim convallis.
-                Mauris sit amet massa vitae tortor condimentum lacinia quis vel.
+                We believe that technology can bridge the gap between empathy
+                and healing. Our platform merges the power of human connection
+                with the convenience of digital tools, helping you embark on a
+                journey towards inner peace. With expert guidance and
+                interactive resources, you can transform your struggles into
+                stepping stones towards a brighter future.
               </p>
               <div className="flex items-center self-start mt-2"></div>
             </div>
