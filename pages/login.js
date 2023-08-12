12db830df89e09cc7b10471237cbfcbce13e4e48
Subhransu <subhranshumajhi2@gmail.com>
2023-08-13 01:27:35 +0530

    minor UI update

--- pages/login.js
+++ pages/login.js
@@ -5,6 +5,7 @@ import { GoogleIcon } from "@/components/icons";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import Cookies from "js-cookie";
+import Head from "next/head";
 
 const Login = () => {
   const router = useRouter();
@@ -53,93 +54,98 @@ const Login = () => {
   };
 
   return (
-    <div className="w-full min-h-screen flex items-start">
-      <div className="relative w-2/5 h-screen flex flex-col">
-        <Image
-          className="w-full h-full object-cover"
-          src={LoginImage}
-          alt="login-image"
-          priority
-        />
-      </div>
-      <div className="w-3/5 h-screen bg-gray-200 p-20 flex flex-col justify-between">
-        <h1 className="text-md text-black font-bold ">Sukoon</h1>
+    <>
+      <Head>
+        <title>Sukoon | Login</title>
+      </Head>
+      <div className="w-full min-h-screen flex items-start">
+        <div className="relative w-2/5 h-screen flex flex-col">
+          <Image
+            className="w-full h-full object-cover"
+            src={LoginImage}
+            alt="login-image"
+            priority
+          />
+        </div>
+        <div className="w-3/5 h-screen bg-gray-200 p-20 flex flex-col justify-between">
+          <h1 className="text-3xl text-black font-bold ">Sukoon</h1>
 
-        <div className="w-full flex flex-col max-w-[700px]">
-          <div className="w-full flex flex-col mb-2">
-            <h2 className="text-2xl text-black font-bold mb-4">Login</h2>
-            <p className="text-sm text-gray-700 font-semibold">
-              Welcome Back! Please log in to proceed
-            </p>
-          </div>
-          <div className="w-full flex flex-col">
-            <input
-              className="w-full text-black bg-inherit py-2 my-2 border-b border-gray-600 outline-none focus:outline-none focus:border-b-2"
-              type="email"
-              placeholder="Email"
-              name="email"
-              onChange={handleChange}
-            />
-            <input
-              className="w-full text-black bg-inherit py-2 my-2 border-b border-gray-600 outline-none focus:outline-none focus:border-b-2"
-              type={showPassword ? "text" : "password"}
-              placeholder="Password"
-              name="password"
-              onChange={handleChange}
-            />
-          </div>
-          <div className="w-full flex item-center justify-between my-2">
-            <label className="flex items-center text-sm">
-              Show Password
+          <div className="w-full flex flex-col max-w-[700px]">
+            <div className="w-full flex flex-col mb-2">
+              <h2 className="text-2xl text-black font-bold mb-4">Login</h2>
+              <p className="text-sm text-gray-700 font-semibold">
+                Welcome Back! Please log in to proceed
+              </p>
+            </div>
+            <div className="w-full flex flex-col">
               <input
-                className="mx-1 mt-1"
-                type="checkbox"
-                checked={showPassword}
-                onChange={handleToggle}
+                className="w-full text-black bg-inherit py-2 my-2 border-b border-gray-600 outline-none focus:outline-none focus:border-b-2"
+                type="email"
+                placeholder="Email"
+                name="email"
+                onChange={handleChange}
               />
-            </label>
-            <p className="text-sm font-semibold underline underline-offset-2 cursor-pointer my-2">
-              Forgot Password
+              <input
+                className="w-full text-black bg-inherit py-2 my-2 border-b border-gray-600 outline-none focus:outline-none focus:border-b-2"
+                type={showPassword ? "text" : "password"}
+                placeholder="Password"
+                name="password"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="w-full flex item-center justify-between my-2">
+              <label className="flex items-center text-sm">
+                Show Password
+                <input
+                  className="mx-1 mt-1"
+                  type="checkbox"
+                  checked={showPassword}
+                  onChange={handleToggle}
+                />
+              </label>
+              <p className="text-sm font-semibold underline underline-offset-2 cursor-pointer my-2">
+                Forgot Password
+              </p>
+            </div>
+            {err && (
+              <div className="w-full flex item-center justify-center my-2 text-red-600 font-semibold">
+                {err}
+              </div>
+            )}
+            <div className="w-full flex items-center justify-center my-2">
+              <button
+                className="w-1/3 rounded-md p-2 text-white text-center bg-black"
+                onClick={handleSubmit}
+              >
+                Login
+              </button>
+            </div>
+
+            <div className="w-full flex items-center justify-center relative">
+              <div className="w-full h-[1.5px] bg-gray-400 my-4"></div>
+            </div>
+            <div className="w-full flex items-center justify-center my-2">
+              <div className="w-1/3 rounded-md p-2 text-black text-center bg-white flex items-center justify-center cursor-pointer">
+                <div className="w-6 h-6 mr-2">
+                  <GoogleIcon />
+                </div>
+                <span>Sign in with Google</span>
+              </div>
+            </div>
+          </div>
+          <div className="w-full flex items-center justify-center max-w-[700px]">
+            <p className="text-sm font-normal text-gray-600">
+              Don&apos;t have an account?&nbsp;
+              <Link href="/register">
+                <span className="font-semibold underline underline-offset-2 cursor-pointer">
+                  Sign up here
+                </span>
+              </Link>
             </p>
           </div>
-          {err && (
-            <div className="w-full flex item-center justify-center my-2 text-red-600 font-semibold">
-              {err}
-            </div>
-          )}
-          <div className="w-full flex items-center justify-center my-2">
-            <button
-              className="w-1/3 rounded-md p-2 text-white text-center bg-black"
-              onClick={handleSubmit}
-            >
-              Login
-            </button>
-          </div>
-
-          <div className="w-full flex items-center justify-center relative">
-            <div className="w-full h-[1.5px] bg-gray-400 my-4"></div>
-          </div>
-          <div className="w-full flex items-center justify-center my-2">
-            <div className="w-1/3 rounded-md p-2 text-black text-center bg-white flex items-center justify-center cursor-pointer">
-              <div className="w-6 h-6 mr-2">
-                <GoogleIcon />
-              </div>
-              <span>Sign in with Google</span>
-            </div>
-          </div>
-        </div>
-        <div className="w-full flex items-center justify-center max-w-[700px]">
-          <p className="text-sm font-normal text-gray-600">
-            Don&apos;t have an account?&nbsp;
-            <Link href="/register">
-              <span className="font-semibold underline underline-offset-2 cursor-pointer">
-                Sign up here
-              </span>
-            </Link>
-          </p>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- pages/register.js
+++ pages/register.js
@@ -5,6 +5,7 @@ import RegisterImage from "@/public/assets/signup-art.jpg";
 import { GoogleIcon } from "@/components/icons";
 import Link from "next/link";
 import Cookies from "js-cookie";
+import Head from "next/head";
 
 const Register = () => {
   const router = useRouter();
@@ -38,108 +39,114 @@ const Register = () => {
       );
       const data = await res.json();
       if (res.status === 201) {
+        const token = data.user_token;
         Cookies.set("sessionToken", token, { expires: 7, secure: true });
         router.push("/");
       } else if (res.status === 409) {
         setErr(data.message);
       }
     } catch (error) {
-      setErr(data.message);
+      console.error(error);
     }
   };
 
   return (
-    <div className="w-full min-h-screen flex items-start">
-      <div className="relative w-2/5 h-screen flex flex-col">
-        <Image
-          className="w-full h-full object-cover"
-          src={RegisterImage}
-          alt="register-image"
-          priority
-        />
-      </div>
-      <div className="w-3/5 h-screen bg-gray-200 p-20 flex flex-col justify-between">
-        <h1 className="text-md text-black font-bold ">Sukoon</h1>
+    <>
+      <Head>
+        <title>Sukoon | Register</title>
+      </Head>
+      <div className="w-full min-h-screen flex items-start">
+        <div className="relative w-2/5 h-screen flex flex-col">
+          <Image
+            className="w-full h-full object-cover"
+            src={RegisterImage}
+            alt="register-image"
+            priority
+          />
+        </div>
+        <div className="w-3/5 h-screen bg-gray-200 p-20 flex flex-col justify-between">
+          <h1 className="text-3xl text-black font-bold ">Sukoon</h1>
 
-        <div className="w-full flex flex-col max-w-[700px]">
-          <div className="w-full flex flex-col mb-2">
-            <h2 className="text-2xl text-black font-bold mb-4">Register</h2>
-            <p className="text-sm text-gray-700 font-semibold">
-              Welcome, Create an account to proceed
+          <div className="w-full flex flex-col max-w-[700px]">
+            <div className="w-full flex flex-col mb-2">
+              <h2 className="text-2xl text-black font-bold mb-4">Register</h2>
+              <p className="text-sm text-gray-700 font-semibold">
+                Welcome, Create an account to proceed
+              </p>
+            </div>
+            <div className="w-full flex flex-col">
+              <input
+                className="w-full text-black bg-inherit py-2 my-2 border-b border-gray-600 outline-none focus:outline-none focus:border-b-2"
+                type="text"
+                placeholder="Username"
+                name="username"
+                onChange={handleChange}
+              />
+              <input
+                className="w-full text-black bg-inherit py-2 my-2 border-b border-gray-600 outline-none focus:outline-none focus:border-b-2"
+                type="email"
+                placeholder="Email"
+                name="email"
+                onChange={handleChange}
+              />
+              <input
+                className="w-full text-black bg-inherit py-2 my-2 border-b border-gray-600 outline-none focus:outline-none focus:border-b-2"
+                type={showPassword ? "text" : "password"}
+                placeholder="Password"
+                name="password"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="w-full flex item-center justify-between my-2">
+              <label className="flex items-center text-sm">
+                Show Password
+                <input
+                  className="mx-1 mt-1"
+                  type="checkbox"
+                  checked={showPassword}
+                  onChange={handleToggle}
+                />
+              </label>
+            </div>
+            {err && (
+              <div className="w-full flex item-center justify-center my-2 text-red-600 font-semibold">
+                {err}
+              </div>
+            )}
+            <div className="w-full flex items-center justify-center my-2">
+              <button
+                className="w-1/3 rounded-md p-2 text-white text-center bg-black"
+                onClick={handleSubmit}
+              >
+                Register
+              </button>
+            </div>
+
+            <div className="w-full flex items-center justify-center relative">
+              <div className="w-full h-[1.5px] bg-gray-400 my-4"></div>
+            </div>
+            <div className="w-full flex items-center justify-center my-2">
+              <div className="w-1/3 rounded-md p-2 text-black text-center bg-white flex items-center justify-center cursor-pointer">
+                <div className="w-6 h-6 mr-2">
+                  <GoogleIcon />
+                </div>
+                <span>Sign up with Google</span>
+              </div>
+            </div>
+          </div>
+          <div className="w-full flex items-center justify-center max-w-[700px]">
+            <p className="text-sm font-normal text-gray-600">
+              Already have an account?&nbsp;
+              <Link href="/login">
+                <span className="font-semibold underline underline-offset-2 cursor-pointer">
+                  Sign in here
+                </span>
+              </Link>
             </p>
           </div>
-          <div className="w-full flex flex-col">
-            <input
-              className="w-full text-black bg-inherit py-2 my-2 border-b border-gray-600 outline-none focus:outline-none focus:border-b-2"
-              type="text"
-              placeholder="Username"
-              name="username"
-              onChange={handleChange}
-            />
-            <input
-              className="w-full text-black bg-inherit py-2 my-2 border-b border-gray-600 outline-none focus:outline-none focus:border-b-2"
-              type="email"
-              placeholder="Email"
-              name="email"
-              onChange={handleChange}
-            />
-            <input
-              className="w-full text-black bg-inherit py-2 my-2 border-b border-gray-600 outline-none focus:outline-none focus:border-b-2"
-              type={showPassword ? "text" : "password"}
-              placeholder="Password"
-              name="password"
-              onChange={handleChange}
-            />
-          </div>
-          <div className="w-full flex item-center justify-between my-2">
-            <label className="flex items-center text-sm">
-              Show Password
-              <input
-                className="mx-1 mt-1"
-                type="checkbox"
-                checked={showPassword}
-                onChange={handleToggle}
-              />
-            </label>
-          </div>
-          {err && (
-            <div className="w-full flex item-center justify-center my-2 text-red-600 font-semibold">
-              {err}
-            </div>
-          )}
-          <div className="w-full flex items-center justify-center my-2">
-            <button
-              className="w-1/3 rounded-md p-2 text-white text-center bg-black"
-              onClick={handleSubmit}
-            >
-              Register
-            </button>
-          </div>
-
-          <div className="w-full flex items-center justify-center relative">
-            <div className="w-full h-[1.5px] bg-gray-400 my-4"></div>
-          </div>
-          <div className="w-full flex items-center justify-center my-2">
-            <div className="w-1/3 rounded-md p-2 text-black text-center bg-white flex items-center justify-center cursor-pointer">
-              <div className="w-6 h-6 mr-2">
-                <GoogleIcon />
-              </div>
-              <span>Sign up with Google</span>
-            </div>
-          </div>
-        </div>
-        <div className="w-full flex items-center justify-center max-w-[700px]">
-          <p className="text-sm font-normal text-gray-600">
-            Already have an account?&nbsp;
-            <Link href="/login">
-              <span className="font-semibold underline underline-offset-2 cursor-pointer">
-                Sign in here
-              </span>
-            </Link>
-          </p>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
