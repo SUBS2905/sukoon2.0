9c7c18b9b161841e681beafcbbfbdb8f1b644f10
Subhransu <subhranshumajhi2@gmail.com>
2023-08-29 10:53:49 +0530

    add format date util

---
+++ utils/utils.js
@@ -0,0 +1,10 @@
+export function formatDate(date){
+    const dateObject = new Date(date);
+
+    const day = dateObject.getDate().toString().padStart(2, '0');
+    const month = (dateObject.getMonth() + 1).toString().padStart(2, '0');
+    const year = dateObject.getFullYear();
+
+    const formattedDate = `${day}-${month}-${year}`;
+    return formattedDate;
+}
