3d8dbbb62f4feae6c44d5a1130be19388e3d5212
Subhransu <subhranshumajhi2@gmail.com>
2023-10-20 22:51:04 +0530

    responsive homepage

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import React from "react";
 const Layout = ({ children, className = "" }) => {
   return (
     <div
-      className={`w-full h-full z-0 inline-block p-32 bg-gray-200 ${className}`}
+      className={`w-full h-full z-0 inline-block bg-gray-200 ${className}`}
     >
       {children}
     </div>
--- components/LayoutDark.js
+++ components/LayoutDark.js
@@ -3,7 +3,7 @@ import React from "react";
 const LayoutDark = ({ children, className = "" }) => {
   return (
     <div
-      className={`w-full h-full z-0 inline-block p-32 bg-gray-700 ${className}`}
+      className={`w-full h-full z-0 inline-block bg-gray-700 ${className}`}
     >
       {children}
     </div>
